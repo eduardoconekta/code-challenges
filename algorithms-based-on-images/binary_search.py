7da5e1193fc4d7a2d9d69316f667e7ce9467fcea
Eduardo Enriquez <eduardo@MacBook-Air-de-Eduardo-E.local>
2018-04-11 00:52:07 -0500

    Binary search

---
+++ algorithms-based-on-images/binary_search.py
@@ -0,0 +1,30 @@
+from time import sleep
+class BinarySort(object):
+  def sort(self, obj, key):
+    mid = (len(obj)/2) + (len(obj) % 2)
+    while True:
+        if obj[mid] == key:
+            break
+        elif key < obj[mid]: 
+            # print "{} < {}".format(key,obj[mid])
+            min = mid - mid
+            mid = (len(obj[min:mid])/2) + (len(obj[min:mid]) % 2)
+        elif  key > obj[mid]:  
+            # print "{} > {}".format(key,obj[mid])
+            min = mid
+            max = (len(obj[mid:len(obj)]) / 2) + (len(obj[min:mid]) % 2)
+            mid += max
+        sleep(3)
+    return obj[mid]
+
+     
+  def __init__(self, arg=None):
+    super(BinarySort, self).__init__()
+    self.arg = arg
+
+
+
+arr = [0,1,2,4,4,5,6,7,8,9]
+key = 3
+binary = BinarySort() 
+binary.sort(arr,key)
