4b2299974ca94b3c0e317d8921bdbdcfd3ce681a
Eduardo Enriquez <eduardo@MacBook-Air-de-Eduardo-E.local>
2018-04-29 22:09:52 -0500

    ooyala continue challenge

---
+++ ooyala/Ooyala.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Escribe una función que acepte dos entradas. 
+Cada entrada es una lista de objetos, 
+donde cada objeto tiene los atributos timestamp y value (ambos son enteros). 
+Cada lista de entrada está ordenada por la clave timestamp, 
+en orden ascendente. 
+Tu función debe unir los elementos de ambas listas en una lista nueva, manteniéndola ordenada.
+Además, si dos o más objetos tienen el mismo valor timestamp, 
+entonces junta los objetos en uno solo, cuyo value es el resultado de la suma de cada value.
+'''
+from time import sleep
+class Ooyala:
+  def merge_sort(self, l1, l2):
+    result = []
+    if self.__is_not_ascendent_list(l1) and self.__is_not_ascendent_list(l2):
+      raise ValueError('list 1 or list 2 are not ascendent list`s.')
+    if self.__is_last_minor(l1[-1:], l2):
+      result.append(l1)
+      result.append(l2)
+    else:
+      c1 = 0
+      c2 = 0
+      while True:
+        if c1 == len(l1) and c2 == len(l2):
+          c1 = 0
+          c2 = 0
+          if self.__is_not_ascendent_list(result):
+            break
+        if l1[c1] == l2[c2]:
+          sum_values = l1[c1]['value'] + l2[c2]['value']
+          result.append({ 'timestamp': l1[c1]['timestamp'], 'value': sum_values })
+        elif l1[c1] < l2[c2]: 
+          result.append(l1[c1])
+          result.append(l2[c2])
+        elif l1[c1] > l2[c2]: 
+          result.append(l2[c2])
+          result.append(l1[c1])
+        c1 +=1
+        c2 +=1
+      
+    return self.__sort(result)
+
+  def __sort(self, l):
+    done = True
+    while done:
+      for i in xrange(0 , len(l) - 1):
+        if l[i] > l[i + 1]:
+          tmp_pos = l[i + 1]
+          l[i + 1 ] = l[i]
+          l[i] = tmp_pos
+          done = False
+    return l
+
+  def __is_last_minor(self, last, l):
+    list_length = len(l)
+    flag = True
+    for i in xrange(0, list_length - 1):
+      if last > l[i]: 
+        flag = False
+    return flag
+
+  def __is_not_ascendent_list(self, l):
+    list_length = len(l)
+    for i in xrange(0, list_length  - 1):
+      if l[i]['timestamp'] > l[i + 1]['timestamp'] :
+        return True
+
+    return False
+
+
+
+     
--- ooyala/challenge_test.py
+++ ooyala/challenge_test.py
@@ -1,6 +1,15 @@
 import nose
-from Ooyala import Ooyala
+import Ooyala
 
+# [{'timestamp': 1523830469, 'value': 246}, 
+# {'timestamp': 1623830469, 'value': 321}, 
+# {'timestamp': 2023830469, 'value': 321}, 
+# {'timestamp': 1723830469, 'value': 345}, 
+# {'timestamp': 2123830469, 'value': 345}, 
+# {'timestamp': 1823830469, 'value': 543}, 
+# {'timestamp': 2223830469, 'value': 543}, 
+# {'timestamp': 1923830469, 'value': 567}, 
+# {'timestamp': 2323830469, 'value': 567}]
 
 list_object_1 = [
     {'timestamp': 1523830469, 'value': 123},
@@ -27,41 +36,52 @@ list_object_1_wrong = [
 ]
 
     
+def test_sum_eq_values():
+    ooyala_sorter = Ooyala.Ooyala()
+    res           = ooyala_sorter.merge_sort(
+        list_object_1, 
+        list_object_2
+    )
+    print res
+    sum_first_values = list_object_1[0]['value'] + list_object_2[0]['value']
+    assert res[0]['value'] == sum_first_values
+
 def test_normal_behaviour():
-    ooyala_sorter = Ooyala()
-    res           = ooyala_sorter.sort(
+    ooyala_sorter = Ooyala.Ooyala()
+    res           = ooyala_sorter.merge_sort(
         list_object_1, 
         list_object_2
     )
     assert res != None
     assert is_well_sorted(res)
-    assert not_duplicated_indexes(res)
+    assert not duplicated_indexes(res)
+
 
 def test_randomenes_values():
     assert True
 
 def test_wrong_input_values():
-    ooyala_sorter = Ooyala()
-    res           = ooyala_sorter.sort(
+    ooyala_sorter = Ooyala.Ooyala()
+    res           = ooyala_sorter.merge_sort(
         list_object_1_wrong, 
         list_object_2
     )
     assert res != None
     assert not is_well_sorted(res)
+    assert not duplicated_indexes(res)
 
 
 def is_well_sorted(l):
     list_length = len(l)
     for i in xrange(0,  list_length -1):
-      if l[i]['timestamp'] < l[i + 1]['timestamp'] :
+      if l[i]['timestamp'] <= l[i + 1]['timestamp'] :
         return False
     return True
 
-def not_duplicated_indexes(l):
+def duplicated_indexes(l):
     list_length = len(l)
     for i in xrange(0, list_length):
         for j in xrange(0, list_length -1):
-            print "duplicated {} {}".format(l[i]['timestamp'],l[j + 1]['timestamp'])
             if l[i]['timestamp'] == l[j+1]['timestamp']:
-                return False
-    return True
+                return True
+    return False
