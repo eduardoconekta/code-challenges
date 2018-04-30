aa52983eb681ce02317ca5950decbed1e2b48703
Eduardo Enriquez <eduardo@MacBook-Air-de-Eduardo-E.local>
2018-04-29 23:06:40 -0500

    Feature for mixed list  values

--- ooyala/challenge_test.py
+++ ooyala/challenge_test.py
@@ -1,15 +1,5 @@
 import nose
-import Ooyala
-
-# [{'timestamp': 1523830469, 'value': 246}, 
-# {'timestamp': 1623830469, 'value': 321}, 
-# {'timestamp': 2023830469, 'value': 321}, 
-# {'timestamp': 1723830469, 'value': 345}, 
-# {'timestamp': 2123830469, 'value': 345}, 
-# {'timestamp': 1823830469, 'value': 543}, 
-# {'timestamp': 2223830469, 'value': 543}, 
-# {'timestamp': 1923830469, 'value': 567}, 
-# {'timestamp': 2323830469, 'value': 567}]
+from Ooyala import Ooyala
 
 list_object_1 = [
     {'timestamp': 1523830469, 'value': 123},
@@ -35,53 +25,57 @@ list_object_1_wrong = [
     {'timestamp': 1923830469, 'value': 567}
 ]
 
+list_object_1_mixed = [
+    {'timestamp': 1123830469, 'value': 321},
+    {'timestamp': 1323830469, 'value': 123}, 
+    {'timestamp': 1523830469, 'value': 345},
+    {'timestamp': 1723830469, 'value': 543},
+    {'timestamp': 1923830469, 'value': 567}
+]
+
+list_object_2_mixed = [
+    {'timestamp': 1223830469, 'value': 321},
+    {'timestamp': 1423830469, 'value': 123},
+    {'timestamp': 1623830469, 'value': 345},
+    {'timestamp': 1823830469, 'value': 543},
+    {'timestamp': 2023830469, 'value': 567}
+]
+
     
 def test_sum_eq_values():
-    ooyala_sorter = Ooyala.Ooyala()
+    ooyala_sorter = Ooyala()
     res           = ooyala_sorter.merge_sort(
         list_object_1, 
         list_object_2
     )
-    print res
     sum_first_values = list_object_1[0]['value'] + list_object_2[0]['value']
     assert res[0]['value'] == sum_first_values
+    assert ooyala_sorter.is_well_sorted(res)
 
 def test_normal_behaviour():
-    ooyala_sorter = Ooyala.Ooyala()
+    ooyala_sorter = Ooyala()
     res           = ooyala_sorter.merge_sort(
         list_object_1, 
         list_object_2
     )
     assert res != None
-    assert is_well_sorted(res)
-    assert not duplicated_indexes(res)
+    assert ooyala_sorter.is_well_sorted(res)
 
-
-def test_randomenes_values():
-    assert True
+def test_normal_behaviour_mixed_values():
+    ooyala_sorter = Ooyala()
+    res           = ooyala_sorter.merge_sort(
+        list_object_1_mixed, 
+        list_object_2_mixed
+    )
+    assert res != None
+    assert ooyala_sorter.is_well_sorted(res)
 
 def test_wrong_input_values():
-    ooyala_sorter = Ooyala.Ooyala()
+    ooyala_sorter = Ooyala()
     res           = ooyala_sorter.merge_sort(
         list_object_1_wrong, 
         list_object_2
     )
     assert res != None
-    assert not is_well_sorted(res)
-    assert not duplicated_indexes(res)
-
-
-def is_well_sorted(l):
-    list_length = len(l)
-    for i in xrange(0,  list_length -1):
-      if l[i]['timestamp'] <= l[i + 1]['timestamp'] :
-        return False
-    return True
-
-def duplicated_indexes(l):
-    list_length = len(l)
-    for i in xrange(0, list_length):
-        for j in xrange(0, list_length -1):
-            if l[i]['timestamp'] == l[j+1]['timestamp']:
-                return True
-    return False
+    assert not ooyala_sorter.is_well_sorted(res)
+    
